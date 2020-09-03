04ce3c7be215c78873a2caf8e335dc20efe2ee5f
financedog <69750586+financedog@users.noreply.github.com>
2020-09-03 14:06:37 +0900

    Update instalike.py
    headless削除

--- instalike.py
+++ instalike.py
@@ -12,8 +12,7 @@ import pyautogui
 # herokuのchromedriverのPATHを指定
 driver_path = '/app/.chromedriver/bin/chromedriver'
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-#※headlessにしている
+
 driver = webdriver.Chrome(options=options, executable_path=driver_path)
 
 
